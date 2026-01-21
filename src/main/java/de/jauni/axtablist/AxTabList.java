7038b1ee163ab5b589cb8b2430145c3d8fe11e66
JauniKappauni <social.jauni@gmail.com>
2026-01-21 22:51:19 +0100

    init: initialized project with basic structure

---
+++ src/main/java/de/jauni/axtablist/AxTabList.java
@@ -0,0 +1,17 @@
+package de.jauni.axtablist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxTabList extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
